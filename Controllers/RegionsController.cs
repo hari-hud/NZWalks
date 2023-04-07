6cc62853b536fb1691257c96e6ae4b24dfe4dd03
Haribhau Hud <hhud@nvidia.com>
2023-04-07 22:59:53 +0530

    Add DTO to getAll method

--- Controllers/RegionsController.cs
+++ Controllers/RegionsController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using NZWalks.Data;
 using NZWalks.Models.Domain;
+using NZWalks.Models.DTO;
 
 namespace NZWalks.Controllers;
 
@@ -18,8 +19,25 @@ public class RegionsController : ControllerBase
     [HttpGet]
     public IActionResult GetAll()
     {
+        // Get Data from Database Model - Domain Model
         var regions = dbContext.Regions.ToList();
-        return Ok(regions); 
+
+        // Map domain model to DTO
+        // This does not expose DB layer to view
+        var regionDto = new List<RegionDto>();
+        foreach(var region in regions)
+        {
+            regionDto.Add(new RegionDto()
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                RegionImageUrl = region.RegionImageUrl
+            });
+        }
+
+        // Return DTO
+        return Ok(regionDto); 
     }
 
     [HttpGet]
@@ -29,7 +47,7 @@ public class RegionsController : ControllerBase
         // var region = dbContext.Regions.Find(id); 
         // OR
         var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
-        
+
         if (region == null) 
         {
             return NotFound();
