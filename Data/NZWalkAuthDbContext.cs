954b03d86ca279a98cd16eed4c5e65d9797e57e4
Haribhau Hud <hhud@nvidia.com>
2023-04-18 08:43:03 +0530

    Seed data and run ef migration

--- Data/NZWalkAuthDbContext.cs
+++ Data/NZWalkAuthDbContext.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using NZWalks.Models.Domain;
@@ -9,6 +10,34 @@ namespace NZWalks.Data
         public NZWalkAuthDbContext(DbContextOptions<NZWalkAuthDbContext> options) : base(options)
         {
             
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerRoleId = "a07cbcfd-aa7c-42a9-a32d-9fffe3978de1";
+            var writerRoleId = "15b0f610-32b1-4bae-8c9f-f068332fb490";
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()
+                },
+                new IdentityRole
+                {
+                    Id = writerRoleId,
+                    ConcurrencyStamp = writerRoleId,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper()
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
         }
     }
 }
